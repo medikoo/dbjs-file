0333d7264a9cecd31e3f513f1f9936f12d0caa99
Mariusz Nowak <mariusz@medikoo.com>
2013-08-19 15:45:25 +0200

    Expose upload events on file object

--- client.js
+++ client.js
@@ -1,40 +1,35 @@
 'use strict';
 
-var isError  = require('es5-ext/lib/Error/is-error')
-  , deferred = require('deferred')
-  , file     = require('dbjs/lib/objects')._get('File').prototype;
+var isError = require('es5-ext/lib/Error/is-error')
+  , file    = require('dbjs/lib/objects')._get('File').prototype;
 
 module.exports = function (FormData, XMLHttpRequest, File, url) {
 	file._$construct.$$setValue(function (file) {
-		var fd, xhr, def = deferred();
+		var fd, xhr, onError;
 
 		fd = new FormData();
 		xhr = new XMLHttpRequest();
 		fd.append('file', file);
 		fd.append('id', this._id_);
 
+		onError = function (e) {
+			this.emit('error', e);
+			this.delete();
+		}.bind(this);
+
 		xhr.open('POST', url, true);
 		xhr.onload = function (data) {
 			if (isError(data)) {
-				def.reject(data);
+				onError(data);
 			} else if ((xhr.status < 200) || (xhr.status >= 300)) {
-				def.reject(new Error(xhr.responseText));
-			} else {
-				def.resolve(data && data.currentTarget);
+				onError(new Error(xhr.responseText));
 			}
 		};
-		xhr.onerror = function () { def.reject(new Error("Error occured")); };
-		xhr.onabort = function () { def.reject(new Error("Operation aborted")); };
+		xhr.onerror = function () { onError(new Error("Error occured")); };
+		xhr.onabort = function () { onError(new Error("Operation aborted")); };
 		xhr.upload.onabort = xhr.onabort;
-		xhr.upload.onprogress = function (e) {
-			def.promise.emit('progress', e);
-		};
+		xhr.upload.onprogress = this.emit.bind(this, 'upload-progress');
 		xhr.send(fd);
-		def.promise.xhr = xhr;
-		def.promise.done(null, function (err) {
-			this.delete();
-			throw err;
-		}.bind(this));
 
 		this.name = file.name;
 		this.type = file.type;
